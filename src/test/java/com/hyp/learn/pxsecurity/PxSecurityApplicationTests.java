1633aa7b4eb6fe34025f0c95134c9197f2bf6551
hanyunpeng <m13839441583@163.com>
2020-02-07 21:24:48 +0800

    first commit

---
+++ src/test/java/com/hyp/learn/pxsecurity/PxSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyp.learn.pxsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PxSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
